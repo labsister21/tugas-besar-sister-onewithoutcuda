6f4ddcda83ce1d140d48229d38912c364ee637cc
Otzzu <mesachharmasendro@gmail.com>
2025-06-03 17:33:51 +0700

    feat: change compose and run.sh

--- README.md
+++ README.md
@@ -8,10 +8,17 @@ docker run --rm --name node1 --net raft --cap-add=NET_ADMIN raft-server node1 80
 
 docker run --rm --name node2 --net raft --cap-add=NET_ADMIN raft-server node2 8082 node2 node1:8081,node3:8083,node4:8084
 
- docker run --rm --name node3 --net raft --cap-add=NET_ADMIN raft-server node3 8083 node3 node1:8081,node2:8082,node4:8084
+docker run --rm --name node3 --net raft --cap-add=NET_ADMIN raft-server node3 8083 node3 node1:8081,node2:8082,node4:8084
 
-  docker run --rm --name node4 --net raft --cap-add=NET_ADMIN raft-server node4 8083 node4 node1:8081,node2:8082,node3:8083
+ docker run --rm --name node4 --net raft --cap-add=NET_ADMIN raft-server node4 8083 node4 node1:8081,node2:8082,node3:8083
 
-  docker run -it --rm --name raft_client --net raft --cap-add=NET_ADMIN raft-client node1:8081
+docker run -it --rm --name raft_client --net raft --cap-add=NET_ADMIN raft-client node1:8081
 
-  docker run -it --rm --name raft_client --net sister-konsensus_raftnet --cap-add=NET_ADMIN raft-client node1:8081
+docker run -it --rm --name raft_client --net sister-konsensus_raftnet --cap-add=NET_ADMIN raft-client node1:8081
+
+docker exec -it node1 sh
+docker exec -it node2 sh
+docker exec -it node3 sh
+docker exec -it node4 sh
+
+tc qdisc show dev lo
--- run.sh
+++ run.sh
@@ -14,19 +14,17 @@ fi
 echo "Starting 5 Raft server nodes..."
 
 docker run -d --rm --name node1 --net $NETWORK_NAME --cap-add=NET_ADMIN \
-  raft-server node1 8081 node1 node2:8082,node3:8083,node4:8084,node5:8085
+  raft-server node1 8081 node1 node2:8082,node3:8083,node4:8084
 
 docker run -d --rm --name node2 --net $NETWORK_NAME --cap-add=NET_ADMIN \
-  raft-server node2 8082 node2 node1:8081,node3:8083,node4:8084,node5:8085
+  raft-server node2 8082 node2 node1:8081,node3:8083,node4:8084
 
 docker run -d --rm --name node3 --net $NETWORK_NAME --cap-add=NET_ADMIN \
-  raft-server node3 8083 node3 node1:8081,node2:8082,node4:8084,node5:8085
+  raft-server node3 8083 node3 node1:8081,node2:8082,node4:8084
 
 docker run -d --rm --name node4 --net $NETWORK_NAME --cap-add=NET_ADMIN \
-  raft-server node4 8084 node4 node1:8081,node2:8082,node3:8083,node5:8085
+  raft-server node4 8084 node4 node1:8081,node2:8082,node3:8083
 
 docker run -d --rm --name node5 --net $NETWORK_NAME --cap-add=NET_ADMIN \
-  raft-server node5 8085 node5 node1:8081,node2:8082,node3:8083,node4:8084
+  raft-server node5 8085 node5
 
-echo "Launching interactive Raft CLI client (connected to node1:8081)..."
-docker run -it --rm --name raft_client --cap-add=NET_ADMIN  --net $NETWORK_N raft-client node1:8081 
