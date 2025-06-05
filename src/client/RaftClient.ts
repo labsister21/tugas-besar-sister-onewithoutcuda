7eb8498013f4d6717cafacf7b81bc6400534f670
Bodleh <tfprince079@gmail.com>
2025-06-05 22:29:22 +0700

    fix: fix undefined value for set and append with no input value

--- src/client/RaftClient.ts
+++ src/client/RaftClient.ts
@@ -218,6 +218,7 @@ export class RaftClient {
   }
 
   async sendCommand(command: string, key?: string, value?: string): Promise<void> {
+    if (value === undefined) value = '';
     const payload = { command, key, value };
 
     if (!this.currentLeaderAddress) {
@@ -256,7 +257,8 @@ export class RaftClient {
       try {
         const res = await axios.post(`http://${this.currentLeaderAddress}/execute`, payload);
         if (res.data.result !== undefined) {
-          console.log('→', res.data.result);
+          const value = res.data.result ?? ''
+          console.log('→', value)
           return;
         }
       } catch (err: any) {
